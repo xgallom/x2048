50de198d24ba215ba2cb88be478a810abee7019b
Milan Gallo <gallo.milan.jr@gmail.com>
2018-06-27 00:01:15 +0200

    Add GameBoard class
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

---
+++ GameBoard.cpp
@@ -0,0 +1,34 @@
+#include "GameBoard.h"
+
+GameBoard::GameBoard()
+{
+	reset();
+}
+
+void GameBoard::reset()
+{
+	for(int y = 0; y < 4; y++)
+		for(int x = 0; x < 4; x++)
+			m_gameBoard[y][x].clear();
+}
+
+const Entry &GameBoard::entryAt(Position position) const
+{
+	return m_gameBoard[position.y][position.x];
+}
+
+Entry &GameBoard::entryAt(Position position)
+{
+	return m_gameBoard[position.y][position.x];
+}
+
+bool GameBoard::isFull() const
+{
+	for(int y = 0; y < 4; y++)
+		for(int x = 0; x < 4; x++)
+			if(m_gameBoard[y][x].isEmpty())
+				return false;
+
+	return true;
+}
+
---
+++ GameBoard.h
@@ -0,0 +1,21 @@
+#ifndef GAME_BOARD_H__
+#define GAME_BOARD_H__
+
+#include "Position.h"
+#include "Entry.h"
+
+class GameBoard {
+	Entry m_gameBoard[4][4];
+
+public:
+	GameBoard();
+
+	void reset();
+
+	const Entry &entryAt(Position position) const;
+	Entry &entryAt(Position position);
+
+	bool isFull() const;
+};
+
+#endif
