9de0b6767ef8d058d8a2fda8f4e0bea8d9e4293e
Milan Gallo <gallo.milan.jr@gmail.com>
2018-06-27 23:11:29 +0200

    Implement GameViewer
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- GameViewer.cpp
+++ GameViewer.cpp
@@ -1,10 +1,116 @@
 #include "GameViewer.h"
+#include "Game.h"
+
+#include <cstring>
 
 GameViewer::GameViewer(Game &game) :
-	m_game(game)
+	m_game(game),
+	m_terminalWindow(nullptr),
+	m_gameWindow(nullptr)
 {}
+
+bool GameViewer::init()
+{
+	m_terminalWindow = initscr();
+
+	if(!m_terminalWindow)
+		return false;
+
+	Position terminalSize = TerminalSize();
+	m_gameWindow = subwin(m_terminalWindow, 8*4, 8*4, terminalSize.y /2 - 8*2, terminalSize.x /2 - 8*2);
+
+	if(!m_gameWindow) {
+		deinit();
+
+		return false;
+	}
+
+	return true;
+}
+
+void GameViewer::deinit()
+{
+	if(m_terminalWindow)
+		endwin();
+}
 
 void GameViewer::repaint()
 {
+	static GameState::Enum lastValue = GameState::Size;
+
+	GameState::Enum gameState = m_game.gameState();
+
+	if(gameState != lastValue)
+		wclear(m_gameWindow);
+
+	lastValue = gameState;
+
+	switch(gameState) {
+	case GameState::Initializing:
+		renderInitializing();
+		break;
+
+	case GameState::Menu:
+		renderMenu();
+		break;
+
+	case GameState::Running:
+		renderRunning();
+		break;
+
+	case GameState::Loading:
+		renderLoading();
+		break;
+
+	case GameState::Saving:
+		renderSaving();
+		break;
+
+	default:
+		break;
+	}
+
+	wrefresh(m_gameWindow);
+}
+
+void GameViewer::renderInitializing()
+{
+	mvwprintw(m_gameWindow, 8*2, 8*2 - strlen("Initializing x2048...")/2, "Initializing x2048...");
+}
+
+void GameViewer::renderMenu()
+{
+}
+
+void GameViewer::renderRunning()
+{
+}
+
+void GameViewer::renderLoading()
+{
+}
+
+void GameViewer::renderSaving()
+{
+}
+
+Position GameViewer::TerminalSize() const
+{
+	Position retval;
+
+	if(m_terminalWindow)
+		getmaxyx(m_terminalWindow, retval.y, retval.x);
+
+	return retval;
+}
+
+Position GameViewer::GameWindowSize() const
+{
+	Position retval;
+
+	if(m_gameWindow)
+		getmaxyx(m_gameWindow, retval.y, retval.x);
+
+	return retval;
 }
 
--- GameViewer.h
+++ GameViewer.h
@@ -1,16 +1,32 @@
 #ifndef GAME_VIEWER_H__
 #define GAME_VIEWER_H__
 
+#include <ncurses.h>
+#include "Position.h"
+
 class Game;
 
 class GameViewer
 {
 	Game &m_game;
+	WINDOW *m_terminalWindow, *m_gameWindow;
 
 public:
 	GameViewer(Game &game);
 
+	bool init();
+	void deinit();
 	void repaint();
+
+private:
+	void renderInitializing();
+	void renderMenu();
+	void renderRunning();
+	void renderLoading();
+	void renderSaving();
+
+	Position TerminalSize() const;
+	Position GameWindowSize() const;
 };
 
 #endif
