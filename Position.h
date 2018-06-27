d7d95d801407c0bc060a15f3bc32ba7d47ace32f
Milan Gallo <milan.gallo@streamunlimited.com>
2018-06-27 11:40:50 +0200

    Reverse parameter order in Position constructor
    Signed-off-by: Milan Gallo <milan.gallo@streamunlimited.com>

--- Position.h
+++ Position.h
@@ -3,11 +3,13 @@
 
 struct Position {
 	Position() = default;
-	explicit Position(int y, int x) : y(y), x(x) {}
+	explicit Position(int x, int y) : y(y), x(x) {}
 
 	int y = 0, x = 0;
 
 	bool isValid() const { return y >= 0 && y < 4 && x >= 0 && x < 4; }
+
+	Position &operator +=(const Position &rhs) { y += rhs.y; x += rhs.x; return *this; }
 };
 
 #endif
